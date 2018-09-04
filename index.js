a5dc9ebb706c6d6aa89699a7d8c9cbdd0f581ea1
Andrew <me@andrewmiracle.com>
2018-09-04 16:03:04 +0100

    add npm start script and create mongodb murl parse pointing index.js entry file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // import mongoose 
 const mongoose = require('mongoose');
+
+// create connection instance using inbuilt mongoose connect object
+// this is a promise and would require a .then and catch error for debugging 
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+.then(() => console.log('connecting to database successful'))
+.catch(err => console.error('could not connect to mongo DB', err))
