5d75067d2a3ee093c945eb12425f2b1111d420b0
Andrew <me@andrewmiracle.com>
2018-09-07 18:03:42 +0100

    add crud update function

---
+++ crud.js
@@ -0,0 +1,102 @@
+// import mongoose 
+const mongoose = require('mongoose');
+
+
+// add newURl parser to mongoose options 
+const options = {
+  useNewUrlParser: true
+}
+
+// mongoose connect 
+mongoose.connect('mongodb://localhost/mongo-exercises', options)
+  .then(() => console.log('connection to mongo-exercise database succesfull'))
+  .catch(error => console.error('could not connect to mongo DB', error))
+
+
+
+// create or import Courses Schema 
+const courseSchema = mongoose.Schema({
+  name: String,
+  tags: [String],
+  author: String,
+  date: { type: Date, default: Date.now },
+  price: Number,
+  isPublished: Boolean
+})
+
+
+
+// Course Mongoose Model for Courses
+// Create Class which can extend the Schema
+const Courses = mongoose.model('Courses', courseSchema)
+
+
+/*
+IT IS GENERALLY ADVISABLE TO RETURN DATA IN FUNCTIONS AND CALL THEM OR CONSOLE.LOG THROUGH OTHER 
+PROCESSES OR FUNCTIONS, USE ASYNC FUNCTIONS TO MANIPULATE RETURN FROM OTHER ASYNC FUNCTIONS
+*/
+
+
+// update course, using the ID
+async function updateCourse(id) {
+  // Approach: Query First 
+  // findById()
+  // Modify its properties
+  // save()
+
+
+
+  // Approach : Update First
+  // Update directly 
+  // Optionally: get the updated document 
+
+  const course = await Courses.findById(id);
+    if (!course) return;
+
+
+    course.isPublished = true;
+    course.author = 'Yuri Gagarin';
+
+
+  // if (!course) {
+  //   return 
+  // }
+  // else {
+  //   course.isPublished = true;
+  //   course.author = 'Andrew Miracle';
+
+  // }
+  const result = await course.save();
+  console.log(result);
+
+
+  // you can also use the course.set() method to directly update the course database
+  // using Approach 2 as stated above 
+  // let result = course.set({
+  //   isPublished: true,
+  //   author: 'Yuri Gagarin'
+
+  // })
+  
+
+}
+
+
+// call the function within updateCourse
+updateCourse('5a68fdd7bee8ea64649c2777');
+
+
+
+
+// async function updateCourse(id) {
+//   const course = await Courses.findById(id);
+//   if (!course) return;
+
+//   course.isPublished = true;
+//   course.author = 'Yuri Gagarin';
+
+//   const result = await course.save();
+//   console.log(result);
+// }
+
+// updateCourse('5a68fdd7bee8ea64649c2777');
---
+++ exercise_three.js
@@ -0,0 +1,79 @@
+// import mongoose 
+const mongoose = require('mongoose');
+
+
+// add newURl parser to mongoose options 
+const options = {
+  useNewUrlParser: true
+}
+
+// mongoose connect 
+mongoose.connect('mongodb://localhost/mongo-exercises', options)
+  .then(() => console.log('connection to mongo-exercise database succesfull'))
+  .catch(error => console.error('could not connect to mongo DB', error))
+
+
+
+// create or import Courses Schema 
+const courseSchema = mongoose.Schema({
+  name: String,
+  tags: [String],
+  author: String,
+  date: { type: Date, default: Date.now },
+  price: Number,
+  isPublished: Boolean
+})
+
+
+
+// Course Mongoose Model for Courses
+// Create Class which can extend the Schema
+const Courses = mongoose.model('Courses', courseSchema)
+
+
+
+// write async function to retrieve document  properties from the courses collection
+// retrieve all backend courses using the $in comparison operator
+// use the .sort method to sort them by their name
+// async function retrieveCourseBackend(){
+//   const courses = await Courses
+//   .find({tags: { $in: 'backend'} })
+//   .and( {isPublished: true} )
+//   .sort( { name: 1} )
+//   // use select to determine which collection properties to output
+//   .select({ name: 1, author: 1, isPublished: 1 })
+//   console.log(courses);
+// }
+
+// console.time(retrieveCourseBackend());
+
+
+
+/*
+IT IS GENERALLY ADVISABLE TO RETURN DATA IN FUNCTIONS AND CALL THEM OR CONSOLE.LOG THROUGH OTHER 
+PROCESSES OR FUNCTIONS, USE ASYNC FUNCTIONS TO MANIPULATE RETURN FROM OTHER ASYNC FUNCTIONS
+*/
+
+async function retrieveExpensiveCourse() {
+  // Get all the Published Courses that are $15 OR more, 
+  // OR have the word 'by' IN their Title
+  return await Courses
+    .find({ isPublished: true})
+    .or([ 
+      {price: { $gte: 15 }}, 
+      //use regular Expression to find words that have by in it
+      {name: /.*by.*/i} 
+    ])
+    .sort({ price: -1 })
+    // use select to determine which collection properties to output
+    // .select({ name: 1, author: 1, isPublished: 1, price: 1 })
+}
+
+
+async function courseQuery(fn) {
+  console.log(await fn)
+};
+
+
+// call the function within courseQuery
+courseQuery(retrieveExpensiveCourse());
